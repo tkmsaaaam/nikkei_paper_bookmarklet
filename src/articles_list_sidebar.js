d06bf7829cd012f1b45a45749ed7e628d5dc74d4
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-11 11:35:24 +0900

    add mark to current page

--- src/articles_list_sidebar.js
+++ src/articles_list_sidebar.js
@@ -10,13 +10,19 @@ javascript:(
       const parser = new DOMParser();
       const doc = parser.parseFromString(res, "text/html");
       const articles = doc.getElementsByClassName('cmn-article_title');
+      const params = new URLSearchParams(location.search);
+      const id = params.get("ng");
       for (let l = 0; l < articles.length; l++) {
         const articlesElement = articles[l];
         const rawArticle = articlesElement.getElementsByTagName('span')[0].getElementsByTagName('a')[0];
         if (!rawArticle) { continue; }
         const articleTitle = rawArticle.getElementsByTagName('span')[0].getElementsByTagName('span')[0].textContent;
         if (!articleTitle) { continue; }
-        html += `<a href=${rawArticle.href}>${articleTitle.substr(0, 18)}</a><br>`
+        let mark = '';
+        if (rawArticle.href.match(id)) {
+          mark = '==';
+        };
+        html += `<a href=${rawArticle.href}>${mark}${articleTitle.substr(0, 14)}${mark}</a><br>`
       };
       document.getElementsByClassName('infoNikkei')[0].insertAdjacentHTML('afterend', html);
       return ;
