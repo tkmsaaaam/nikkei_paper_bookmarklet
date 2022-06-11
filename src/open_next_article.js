21fb5faa2d7a4e52fc9c4e1109f5314bd5cda3a3
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-11 12:00:22 +0900

    npm init && npm install prettier

--- src/open_next_article.js
+++ src/open_next_article.js
@@ -1,27 +1,31 @@
-javascript:(
-  ()=>{
-    try{
-      if (location.hostname !== 'www.nikkei.com' || !location.pathname.startsWith('/paper/article/')) {
-        return alert('This plugin works on `nikkei.com/paper/article/` only!')
-      };
-      const articleBtnNext = document.getElementsByClassName('m−paper_articleBtn_next')[0];
-      if (articleBtnNext !== undefined) {
-        window.location.href = articleBtnNext.href;
-      } else {
-        const contentsMain = document.getElementById('CONTENTS_MAIN');
-        const cmncTitleElement = contentsMain.getElementsByClassName('cmnc-title')[0];
-        const cmncTitle = cmncTitleElement.textContent;
-        const jsidUrlData = contentsMain.getElementsByTagName('li');
-        for (const j in jsidUrlData) {
-          if(~jsidUrlData[j].textContent.indexOf(cmncTitle)){
-            jsidUrlData[parseInt(j)+1].getElementsByTagName('a')[0].click();
-            break;
-          }
-        };
-      };
-    }catch(e){
-      console.log(e);
-      alert(e);
+javascript: (() => {
+  try {
+    if (
+      location.hostname !== "www.nikkei.com" ||
+      !location.pathname.startsWith("/paper/article/")
+    ) {
+      return alert("This plugin works on `nikkei.com/paper/article/` only!");
     }
+    const articleBtnNext = document.getElementsByClassName(
+      "m−paper_articleBtn_next"
+    )[0];
+    if (articleBtnNext !== undefined) {
+      window.location.href = articleBtnNext.href;
+    } else {
+      const contentsMain = document.getElementById("CONTENTS_MAIN");
+      const cmncTitleElement =
+        contentsMain.getElementsByClassName("cmnc-title")[0];
+      const cmncTitle = cmncTitleElement.textContent;
+      const jsidUrlData = contentsMain.getElementsByTagName("li");
+      for (const j in jsidUrlData) {
+        if (~jsidUrlData[j].textContent.indexOf(cmncTitle)) {
+          jsidUrlData[parseInt(j) + 1].getElementsByTagName("a")[0].click();
+          break;
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    alert(e);
   }
-)();
+})();
