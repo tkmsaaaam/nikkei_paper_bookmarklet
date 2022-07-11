c00738070bafe9cb5243d72c16ff9646b926c3e7
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-07-11 22:59:08 +0900

    separate method

--- src/articles_list_sidebar.js
+++ src/articles_list_sidebar.js
@@ -3,40 +3,84 @@ javascript: (async () => {
 		if (location.hostname !== 'www.nikkei.com') {
 			return alert('This plugin works on `nikkei.com/paper/article/` only!');
 		}
-		const url = `https://www.nikkei.com/paper/`;
-		const res = await fetch(url).then(response => response.text());
-		let html = '';
-		const parser = new DOMParser();
-		const doc = parser.parseFromString(res, 'text/html');
-		const articles = doc.getElementsByClassName('cmn-article_title');
-		const params = new URLSearchParams(location.search);
-		const id = params.get('ng');
-		for (let l = 0; l < articles.length; l++) {
-			const articlesElement = articles[l];
-			const rawArticle = articlesElement
-				.getElementsByTagName('span')[0]
-				.getElementsByTagName('a')[0];
-			if (!rawArticle) {
-				continue;
+
+		const createMark = () => {
+			const params = new URLSearchParams(location.search);
+			insertMark(params.get('ng'));
+		};
+
+		const createArticlesList = doc => {
+			const articleList = [];
+			const articles = doc.getElementsByClassName('cmn-article_title');
+			for (let l = 0; l < articles.length; l++) {
+				const articlesElement = articles[l];
+				const rawArticle = articlesElement
+					.getElementsByTagName('span')[0]
+					.getElementsByTagName('a')[0];
+				if (!rawArticle) continue;
+				const articleTitle = rawArticle
+					.getElementsByTagName('span')[0]
+					.getElementsByTagName('span')[0].textContent;
+				if (!articleTitle) continue;
+				let article = {};
+				article.href = rawArticle.href;
+				article.title = articleTitle.substr(0, 16);
+				articleList.push(article);
 			}
-			const articleTitle = rawArticle
-				.getElementsByTagName('span')[0]
-				.getElementsByTagName('span')[0].textContent;
-			if (!articleTitle) {
-				continue;
+			return articleList;
+		};
+
+		const getArticles = async () => {
+			const url = `https://www.nikkei.com/paper/`;
+			const res = await fetch(url).then(response => response.text());
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(res, 'text/html');
+			return createArticlesList(doc);
+		};
+
+		const createHtml = articleList => {
+			let html = '';
+			for (let i = 0; i < articleList.length; i++) {
+				const article = articleList[i];
+				html += `<a href=${article.href}>${article.title.substr(
+					0,
+					16
+				)}</a><br>`;
 			}
-			let mark = '';
-			if (rawArticle.href.match(id)) {
-				mark = '==';
+			return html;
+		};
+
+		const insertHtml = html => {
+			document
+				.getElementsByClassName('cmn-sub_rightbox')[0]
+				.insertAdjacentHTML('afterbegin', html);
+		};
+
+		const insertMark = id => {
+			const articlesHtml = document
+				.getElementById('articles')
+				.getElementsByTagName('a');
+			for (let i = 0; i < articlesHtml.length; i++) {
+				const articleHtml = articlesHtml[i];
+				const href = new URLSearchParams(articleHtml.href);
+				const ng = href.get('ng');
+				if (ng === id) {
+					return articleHtml.insertAdjacentHTML(
+						'beforebegin',
+						'<a id="marked">=></a>'
+					);
+				}
 			}
-			html += `<a href=${rawArticle.href}>${mark}${articleTitle.substr(
-				0,
-				14
-			)}${mark}</a><br>`;
-		}
-		document
-			.getElementsByClassName('cmn-sub_rightbox')[0]
-			.insertAdjacentHTML('beforebegin', html);
+		};
+
+		const renderArticles = async () => {
+			const articleList = await getArticles();
+			const html = createHtml(articleList);
+			insertHtml(html);
+			createMark();
+		};
+
+		renderArticles('');
 		return;
 	} catch (e) {
 		console.log(e);
